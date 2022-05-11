1828d7e5b6aef44951836b902ddaf088e61c3b37
Saad allah Samadi <ssamadi@e1r13p6.1337.ma>
2022-05-11 19:07:14 +0100

    parsing paramter of url

--- http_par/parsing_request.cpp
+++ http_par/parsing_request.cpp
@@ -9,7 +9,9 @@ void first_line(std::string line, data &save) // ====> GET example.com HTTP/1.1
     // data save;
     std::istringstream iss(line);
 	std::string     buff;
+    std::string delimiter = "&";
     // std::cout << line << std::endl;
+    size_t d;
     for(int i = 0; i < 3; i++)
     {
         iss >> buff;
@@ -17,6 +19,31 @@ void first_line(std::string line, data &save) // ====> GET example.com HTTP/1.1
             save.method = buff;
         else if(i == 1)
             save.path = buff;
+    }
+    std::string tmp(save.path);
+    // std::cout << (d =save.path.find("?"));
+    if ((d =save.path.find("?")) != std::string::npos) // <example> GET /cart.php?list=2&id=2 HTTP/1.1
+    {
+
+        tmp.erase(0, d+1); // &
+    }
+    // std::string s = "scott>=tiger>=mushroom";
+
+    size_t pos = 0;
+    std::string token;
+    while ((pos = tmp.find(delimiter)) != std::string::npos) 
+    {
+        token = tmp.substr(0, pos);
+        save.paramter.push_back(token);
+        std::cout << token << std::endl;
+        tmp.erase(0, pos + delimiter.length());
+        
+        // std::cout << token << std::endl;
+    }
+    std::vector<std::string>::iterator it = save.paramter.begin();
+    for(; it != save.paramter.end(); it++)
+    {
+        std::cout << "pra >> " << *it << std::endl;
     }
     // std::cout << save.method << std::endl << save.path << std::endl;
 }
@@ -35,24 +62,21 @@ void parsing_header(std::fstream &fs, data &d)
         }
         else if (lines.find("Content-Length:") != std::string::npos) // Content-Length: 69
         {
-            // std::cout << "Reached here ==> "  << lines << std::endl;
-            // lines.erase(0, strlen("Content-Length: "));
             d.lenth =  lines.erase(0, strlen("Content-Length: "));
-            // std::cout << "line == >" << lines << std::endl; 
         }
         else if (lines.find("Referer:") != std::string::npos) // Referer: http://127.0.0.1:2082/
         {
-            // std::cout << "Reached here ==> "  << lines << std::endl;
-            // lines.erase(0, strlen("Content-Length: "));
             d.referer = lines.erase(0, strlen("Referer: "));
             // std::cout << "line == >" << lines << std::endl; 
         }
             //std::cout << "size ==> " << lines.length() << std::endl;
-        if (lines.length()) //TODO: check this.
+        if (lines.length() == 1) //TODO: check this.
         {
             break ;
         }
+        // std::cout << "line " << lines << std::endl;
     }
+
 }
 
 
@@ -73,7 +97,7 @@ int main() {
     data data_to_save;
     // fs.open ("ex_3", std::fstream::in | std::fstream::out | std::fstream::app);
     // fs.open ("../just_test", std::fstream::in | std::fstream::out | std::fstream::binary);
-    fs.open ("../just_test", std::fstream::in | std::fstream::out);
+    fs.open ("../Tests/just_test", std::fstream::in | std::fstream::out);
     // fs.open ("POST_REQUEST", std::fstream::in | std::fstream::out | std::fstream::app);
     std::string line_save;
 //    fs << "\r\n\r\n";
